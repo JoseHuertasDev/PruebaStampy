849a0916af033cb1261fe6b5e1da1c8b7bfd0fb4
JoseHuertasDev <70613520+JoseHuertasDev@users.noreply.github.com>
2021-03-17 20:58:45 -0300

    refactor: Añado startup
    Clase para configurar las dependencias y para iniciar el ruteo

---
+++ startup.php
@@ -0,0 +1,31 @@
+<?php
+    require_once './libs/router.class.php';
+    require_once './app/controller/user.controller.php';
+
+    // CONSTANTES PARA RUTEO
+    define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+    define("LOGIN", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/login');
+    define("LOGOUT", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/logout');
+    
+    class Startup{
+        public function init(){
+
+            //Añadimos las dependencias
+            DependencyInjectorEngine::add("UserController","UserController");
+            DependencyInjectorEngine::add("UserView","UserView");
+            DependencyInjectorEngine::addSingleton("UserModel","UserModel");
+
+            $this->startRouter();
+        }   
+
+        private function startRouter(){
+            // creo el router
+            $router = new Router(); 
+            //"encendemos" el router
+            $router->addRoute("login", "GET", "UserController", "Login");
+            $router->route($_GET['action'], $_SERVER['REQUEST_METHOD']); 
+        }
+    }
+
+
+    (new Startup())->init();
